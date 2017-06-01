0e08d9856351c5e380fe66d8a8124027def4a32b
Girdhari Choyal <girdharichoyal@gmail.com>
2017-06-01 13:29:58 +0530

    DB table check if already exists.

--- plugin.php
+++ plugin.php
@@ -429,18 +429,22 @@ function csp_subscribers_install() {
 
 	$table_name = $wpdb->prefix . 'csp_subscribers';
 	
-	$charset_collate = $wpdb->get_charset_collate();
+	if( $wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name ){
+			
+		$charset_collate = $wpdb->get_charset_collate();
 
-	$sql = "CREATE TABLE $table_name (
-		id mediumint(9) NOT NULL AUTO_INCREMENT,
-		fname varchar(250) DEFAULT '' NOT NULL,
-		lname varchar(250) DEFAULT '' NOT NULL,
-		email varchar(250) DEFAULT '' NOT NULL,
-		PRIMARY KEY  (id)
-	) $charset_collate;";
+		$sql = "CREATE TABLE $table_name (
+			id mediumint(9) NOT NULL AUTO_INCREMENT,
+			fname varchar(250) DEFAULT '' NOT NULL,
+			lname varchar(250) DEFAULT '' NOT NULL,
+			email varchar(250) DEFAULT '' NOT NULL,
+			PRIMARY KEY  (id)
+		) $charset_collate;";
 
-	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-	dbDelta( $sql );
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		dbDelta( $sql );
+		
+	}
 	
 }
 
